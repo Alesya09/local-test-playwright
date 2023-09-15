b05f891d4f3f31600d776fa07e88a6bef69e2b89
Alesya09 <annalesh@mail.ru>
2023-09-14 21:03:19 -0500

    prettier

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,25 +1,25 @@
-const {test, expect} = require('@playwright/test')
+const { test, expect } = require("@playwright/test");
 
-test.describe('Authentication & Authorization', () => {
-    test('Sign in with existing credentials', async({ page }) => {
-await page.goto('/user/login')
+test.describe("Authentication & Authorization", () => {
+  test("Sign in with existing credentials", async ({ page }) => {
+    await page.goto("/user/login");
 
-await page.locator('#normal_login_email').fill(process.env.EMAIL)
-await page.locator('#normal_login_password').fill(process.env.PASSWORD)
-await page.locator('button[type="submit"]').click()
+    await page.locator("#normal_login_email").fill(process.env.EMAIL);
+    await page.locator("#normal_login_password").fill(process.env.PASSWORD);
+    await page.locator('button[type="submit"]').click();
 
-await expect(page.locator('.ant-avatar-square')).toBeVisible
-    })
+    await expect(page.locator(".ant-avatar-square")).toBeVisible;
+  });
 
-    test('Sign in with negative credentials password', async({ page }) => {
-        await page.goto('https://coding.pasv.us/user/login')
+  test("Sign in with negative credentials password", async ({ page }) => {
+    await page.goto("https://coding.pasv.us/user/login");
 
-await page.locator('#normal_login_email').fill(process.env.EMAIL)
-await page.locator('#normal_login_password').fill('invalid')
-await page.locator('button[type="submit"]').click()
+    await page.locator("#normal_login_email").fill(process.env.EMAIL);
+    await page.locator("#normal_login_password").fill("invalid");
+    await page.locator('button[type="submit"]').click();
 
-const toast = page.locator('.ant-notification-notice-message')
-await expect(toast).toBeVisible()
-await expect(toast).toHaveText('User login. Fail')
-    })
-})
+    const toast = page.locator(".ant-notification-notice-message");
+    await expect(toast).toBeVisible();
+    await expect(toast).toHaveText("User login. Fail");
+  });
+});
