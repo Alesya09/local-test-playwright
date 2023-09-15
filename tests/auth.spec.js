687cb304342960695392e2e8cbb4af48a7714cb5
Alesya09 <annalesh@mail.ru>
2023-09-14 20:37:47 -0500

    env

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -2,10 +2,10 @@ const {test, expect} = require('@playwright/test')
 
 test.describe('Authentication & Authorization', () => {
     test('Sign in with existing credentials', async({ page }) => {
-await page.goto('https://coding.pasv.us/user/login')
+await page.goto('/user/login')
 
-await page.locator('#normal_login_email').fill('annalesh@mail.ru')
-await page.locator('#normal_login_password').fill('05122006Anna')
+await page.locator('#normal_login_email').fill(process.env.EMAIL)
+await page.locator('#normal_login_password').fill(process.env.PASSWORD)
 await page.locator('button[type="submit"]').click()
 
 await expect(page.locator('.ant-avatar-square')).toBeVisible
@@ -14,8 +14,8 @@ await expect(page.locator('.ant-avatar-square')).toBeVisible
     test('Sign in with negative credentials password', async({ page }) => {
         await page.goto('https://coding.pasv.us/user/login')
 
-await page.locator('#normal_login_email').fill('annalesh@mail.ru')
-await page.locator('#normal_login_password').fill('05122006Ann')
+await page.locator('#normal_login_email').fill(process.env.EMAIL)
+await page.locator('#normal_login_password').fill('invalid')
 await page.locator('button[type="submit"]').click()
 
 const toast = page.locator('.ant-notification-notice-message')
