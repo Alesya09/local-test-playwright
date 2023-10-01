3d9a29f4446977503855e8011b6a114454cde55d
Alesya09 <annalesh@mail.ru>
2023-09-30 22:47:04 -0500

    pom2

--- common/test.js
+++ common/test.js
@@ -1,7 +1,7 @@
 import {test as base, expect} from '@playwright/test'
 import LoginPage from '../pages/login'
 
-const test = base.extend({
+const test= base.extend({
   loginPage: async ({page}, use) => {
     await use(new LoginPage(page))
   },
